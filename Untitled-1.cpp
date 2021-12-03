c04d739dcb75a7cbec224fe028dc046291e16155
longfish-sama <chendangdang@live.com>
2021-12-03 23:01:28 +0800

    21/12/3/2301genkeys

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,22 +1,28 @@
-#include <gmp.h>
-#include <iostream>
 #include "Untitled-1.hpp"
 using namespace std;
 
 int main()
 {
-    mpz_t p, q, n,e,d,phi;
-    mpz_init(p);
-    mpz_init(q);
+    mpz_t p,q,n,e,d,phi;
+    mpz_init_set_str(p,"0",2);
+    mpz_init_set_str(q,"0",2);
     mpz_init(n);
-    genPrime(p);
-    genPrime(q);
-    gmp_printf("%Zx\n", p);
-    gmp_printf("%Zx\n",q);
-    while (p == q)
+    mpz_init(d);
+    mpz_init(phi);
+    mpz_init_set_ui(e, 65537);
+    //生成两个大质数
+    while (!mpz_cmp(p,q))
     {
-        genPrime(q);
+        genPrime(p,q);
     }
+    gmp_printf("p:\n%Zx\n", p);
+    gmp_printf("q:\n%Zx\n", q);
+    //计算d
+    calD(d, p, q, e);
+    gmp_printf("d:\n%Zd\n", d);
+    //计算n
+    mpz_mul(n,p,q);
+    gmp_printf("n:\n%Zd\n", n);
 
     return 0;
 }
--- Untitled-1.hpp
+++ Untitled-1.hpp
@@ -1,34 +1,43 @@
 #include <gmp.h>
 #include <ctime>
 #include <iostream>
+#include <string>
 using namespace std;
 
-void genPrime(mpz_t return_prime)
+void genPrime(mpz_t &return_p, mpz_t &return_q)
 {
     //声明及初始化变量
-    mpz_t p;
+    mpz_t p, q;
     mpz_init(p);
+    mpz_init(q);
     //生成随机数
     gmp_randstate_t rand_state;
     gmp_randinit_default(rand_state);
     gmp_randseed_ui(rand_state, time(NULL));
     mpz_urandomb(p, rand_state, 1024);
+    mpz_urandomb(q, rand_state, 1024);
     //生成比p大的下一个素数
-    mpz_nextprime(return_prime, p);
+    mpz_nextprime(return_p, p);
+    mpz_nextprime(return_q, q);
     mpz_clear(p);
+    mpz_clear(q);
 }
 
-void genD(mpz_t return_d, mpz_t p, mpz_t q, mpz_t e)
+void calD(mpz_t &return_d, mpz_t &p, mpz_t &q, mpz_t &e)
 {
     //初始化
-    mpz_t d, phi;
-    mpz_init(d);
+    mpz_t phi;
     mpz_init(phi);
     //计算欧拉函数phi
     mpz_sub_ui(p, p, 1);
     mpz_sub_ui(q, q, 1);
     mpz_mul(phi, p, q);
     //计算d
-    mpz_invert(d, e, phi);
-    return_d = d;
+    mpz_invert(return_d, e, phi);
+    mpz_clear(phi);
+}
+
+string encryptPKey(string plain_text,mpz_t n,mpz_t e)
+{
+    
 }
