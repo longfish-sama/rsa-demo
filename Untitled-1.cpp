7a7b25b82368082b67010fa1f70fd83010b5cc4f
longfish-sama <chendangdang@live.com>
2021-12-07 00:26:50 +0800

    21/12/7

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,28 +1,40 @@
 #include "Untitled-1.hpp"
+#include "cppcodec-0.2/cppcodec/hex_default_lower.hpp"
 using namespace std;
+using namespace cppcodec;
 
 int main()
 {
-    mpz_t p,q,n,e,d,phi;
-    mpz_init_set_str(p,"0",2);
-    mpz_init_set_str(q,"0",2);
+    mpz_t p, q, n, e, d, phi;
+    mpz_init_set_str(p, "0", HEX_BASE);
+    mpz_init_set_str(q, "0", HEX_BASE);
     mpz_init(n);
     mpz_init(d);
     mpz_init(phi);
     mpz_init_set_ui(e, 65537);
-    //生成两个大质数
-    while (!mpz_cmp(p,q))
+    //生成两个大质数pq
+    while (!mpz_cmp(p, q))
     {
-        genPrime(p,q);
+        genPrime(p, q);
     }
-    gmp_printf("p:\n%Zx\n", p);
-    gmp_printf("q:\n%Zx\n", q);
+    //计算n
+    mpz_mul(n, p, q);
     //计算d
     calD(d, p, q, e);
-    gmp_printf("d:\n%Zd\n", d);
-    //计算n
-    mpz_mul(n,p,q);
-    gmp_printf("n:\n%Zd\n", n);
+    gmp_printf("p:\n%Zx\n", p);
+    cout << "length p = " << mpz_sizeinbase(p, BIN_BASE) << " bits" << endl;
+    gmp_printf("q:\n%Zx\n", q);
+    cout << "length q = " << mpz_sizeinbase(q, BIN_BASE) << " bits" << endl;
+    gmp_printf("d:\n%Zx\n", d);
+    cout << "length d = " << mpz_sizeinbase(d, BIN_BASE) << " bits" << endl;
+    gmp_printf("n:\n%Zx\n", n);
+    cout << "length n = " << mpz_sizeinbase(n, BIN_BASE) << " bits" << endl;
+    string s = "abcdefg";
+    string ss = hex_lower::encode(s.data(),s.length());
+    string en = encryptPKey(ss, n, e);
+    string de = decryptSKey(en, n, d);
+    char *re = new char[de.length()+16];
+    hex_lower::decode(re,de.length(),de.data(),de.length());
 
     return 0;
 }
--- Untitled-1.hpp
+++ Untitled-1.hpp
@@ -2,10 +2,14 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <string.h>
+#define HEX_BASE 16
+#define BIN_BASE 2
+#define LEN 2048
 using namespace std;
 
 void genPrime(mpz_t &return_p, mpz_t &return_q)
-{
+{ //生成两个质数
     //声明及初始化变量
     mpz_t p, q;
     mpz_init(p);
@@ -14,8 +18,8 @@ void genPrime(mpz_t &return_p, mpz_t &return_q)
     gmp_randstate_t rand_state;
     gmp_randinit_default(rand_state);
     gmp_randseed_ui(rand_state, time(NULL));
-    mpz_urandomb(p, rand_state, 1024);
-    mpz_urandomb(q, rand_state, 1024);
+    mpz_urandomb(p, rand_state, LEN / 2);
+    mpz_urandomb(q, rand_state, LEN / 2);
     //生成比p大的下一个素数
     mpz_nextprime(return_p, p);
     mpz_nextprime(return_q, q);
@@ -24,20 +28,68 @@ void genPrime(mpz_t &return_p, mpz_t &return_q)
 }
 
 void calD(mpz_t &return_d, mpz_t &p, mpz_t &q, mpz_t &e)
-{
+{ //计算d
     //初始化
-    mpz_t phi;
+    mpz_t phi, p1, q1;
     mpz_init(phi);
+    mpz_init(p1);
+    mpz_init(q1);
     //计算欧拉函数phi
-    mpz_sub_ui(p, p, 1);
-    mpz_sub_ui(q, q, 1);
-    mpz_mul(phi, p, q);
+    mpz_sub_ui(p1, p, 1);
+    mpz_sub_ui(q1, q, 1);
+    mpz_mul(phi, p1, q1);
     //计算d
     mpz_invert(return_d, e, phi);
     mpz_clear(phi);
+    mpz_clear(p1);
+    mpz_clear(q1);
 }
 
-string encryptPKey(string plain_text,mpz_t n,mpz_t e)
+string encryptPKey(string &plain_text, mpz_t &n, mpz_t &e)
+{ //使用公钥加密
+    mpz_t M, C;
+    //const char* p=plain_text.data();
+    mpz_init_set_str(M, plain_text.data(), HEX_BASE);
+    mpz_init_set_ui(C, 0);
+    //计算密文
+    mpz_powm(C, M, e, n);
+    char *encrypted_str = new char[plain_text.length() + 16];
+    string return_str;
+    mpz_get_str(encrypted_str, HEX_BASE, C);
+    return_str = encrypted_str;
+    mpz_clear(M);
+    mpz_clear(C);
+    return return_str;
+}
+
+string encryptSKey(string plain_text, mpz_t n, mpz_t d)
+{ //使用私钥签名
+    mpz_t M, C;
+    mpz_init_set_str(M, plain_text.data(), HEX_BASE);
+    mpz_init_set_ui(C, 0);
+    //计算密文
+    mpz_powm(C, M, d, n);
+    char *encrypted_str = new char[plain_text.length() + 16];
+    string return_str;
+    mpz_get_str(encrypted_str, HEX_BASE, C);
+    return_str = encrypted_str;
+    mpz_clear(M);
+    mpz_clear(C);
+    return return_str;
+}
+
+string decryptSKey(string &cipher_text, mpz_t &n, mpz_t &d)
 {
-    
+    mpz_t M, C;
+    mpz_init_set_str(C, cipher_text.data(), HEX_BASE);
+    mpz_init_set_ui(M, 0);
+    //
+    mpz_powm(M, C, d, n);
+    char *decrypted_str = new char[cipher_text.length() + 16];
+    string return_str;
+    mpz_get_str(decrypted_str, HEX_BASE, M);
+    return_str = decrypted_str;
+    mpz_clear(M);
+    mpz_clear(C);
+    return return_str;
 }
